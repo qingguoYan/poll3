deb227e491d7e3147b87c2d48d4f967db310e67c
yang <yang@DESKTOP-N39GEQ9>
2018-06-27 10:29:08 +0800

    添加了答案模块

--- src/main/java/com/briup/apps/poll/dao/extend/AnswersVMMapper.java
+++ src/main/java/com/briup/apps/poll/dao/extend/AnswersVMMapper.java
@@ -7,13 +7,6 @@ import com.briup.apps.poll.bean.extend.AnswersVM;
 
 public interface AnswersVMMapper {
 
-	List<AnswersVM>findAll() throws Exception;
-	 AnswersVM findById(long id) throws Exception;
-	 //关键字查询
-	 List<AnswersVM> query(String keywords) throws Exception;
-	 void saveOrUpdate(AnswersVM answer) throws Exception;
-	 void deleteById(long id) throws Exception;
-	 
-	 //批量删除
-	 void batchDelete(List<Long> id) throws Exception;
+	List<AnswersVM> selectAll() throws Exception;
+	
 }
--- src/main/java/com/briup/apps/poll/service/IAnswersService.java
+++ src/main/java/com/briup/apps/poll/service/IAnswersService.java
@@ -3,11 +3,13 @@ package com.briup.apps.poll.service;
 import java.util.List;
 
 import com.briup.apps.poll.bean.Answers;
+import com.briup.apps.poll.bean.extend.AnswersVM;
 
 
 public interface IAnswersService {
 
 	List<Answers>findAll() throws Exception;
+	List<AnswersVM> findAllAnswersVM() throws Exception;
 	 Answers findById(long id) throws Exception;
 	 //关键字查询
 	 List<Answers> query(String keywords) throws Exception;
